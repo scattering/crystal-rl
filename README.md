461995324b97ba591036a5924280e28f2747efa8
abby-e-wilson <abbyw42@gmail.com>
2018-08-06 15:30:56 +0000

    updated deep  Q learning documentation

--- README.md
+++ README.md
@@ -4,20 +4,57 @@ This is a set of Reinforcement Learning algorithms for use with pycrysfml. They 
 
 ## Q Learning Setup
 
+### Pycrysfml
+
 First set up and install pycrysfml. Follow the commands in the instructions file in pycrysfml/doc/Ubuntu_deps.txt
+
+Then, replace pycrysfml/hklgen/sxtal_model.py with the sxtal_model.py file in this repository. This updates the model class to accept empty lists for the observed data, so you can create a model without giving it all of the initial data.
+
+### Code
 
 This code is designed to be run from:
 
      pycrysfml/hklgen/examples/sxtal/crystal-rl/sxtalQLearning.py
-     pycrysfml/hklgen/examples/HOBK/simpleQAlg.py
 
-simpleQAlg.py is a draft which was coverted to sxtalQLearning.py, it is not inteded to be functional.
-
-The sxtalQLearning program is a simple Q Learning algorithm for use with the praseodymium nicolate data. Replace the prnio.cfl file in pycrysfml/hklgen/examples/sxtal/prnio.cfl with the prnio_optimized.cfl file
-This file contains optimized atomic position values from fitting the data with FullProf. It will allow you to get better fits on the praseodymium nicolate crsytal model when only fitting a single parameter.
+The sxtalQLearning program is a simple Q Learning algorithm for use with the praseodymium nickolate data. Replace the prnio.cfl file in pycrysfml/hklgen/examples/sxtal/prnio.cfl with the prnio_optimized.cfl file
+This file contains optimized atomic position values from fitting the data with FullProf. It will allow you to get better fits on the praseodymium nickolate crsytal model when only fitting a single parameter.
 Adjust the files to write output to write to your desired file names and run the code with the command:
 
     python sxtalQLearning.py
 
 The program will write out a log of the rewards earned by the agent over time. It also writes the Q table to a file, so that you can load the Q table and continue training from where you left off.
 
+## Deep Q Learning Setup
+
+### Tensorforce
+
+Tensorforce relies on Tensorflow. Use a docker container with tensorforce to most easily set this up. You need to have nvidia-docker installed for this to work.
+Run: 
+
+    $  nvidia-docker run -it tensorflow/tensorflow:latest-gpu /bin/bash
+
+This creates a docker container with tensorflow set up to use the gpu. Within the container, install TensorForce
+
+    $ pip install tensorforce
+
+For more instructions regarding tensorforce setup, see https://github.com/reinforceio/tensorforce
+
+### Pycrysfml
+
+Use above instructions in Q Learning section.
+
+### Code
+
+The training code will import TensorForce and set up an agent, model, and runner to train a model on the environment. You have to pass in an agent configuration.
+A Deep Q Learning agent configuration is included here, got to https://github.com/reinforceio/tensorforce/tree/master/examples/configs for more examples. 
+
+Place the files within pycrysfml, or define your filepath at the top of each file so they can find the data files
+
+    pycrysfml/hklgen/examples/sxtal/crystal-rl/pycrysfmlTraining.py
+    pycrysfml/hklgen/examples/sxtal/crystal-rl/pycrysfmlEnvironment.py
+
+Run the training:
+
+    python pycrysfmlTraining.py -a dqn_agent.json
+
+Substitute your own agent after the -a tag to use a different one.
--- pycrysfmlEnvironment.py
+++ pycrysfmlEnvironment.py
@@ -29,22 +29,18 @@ class PycrysfmlEnvironment(Environment):
 
     def __init__(self,  observedFile, infoFile, backgFile=None, sxtal=True):
 
-        if sxtal:
+        #Read data
+        self.spaceGroup, self.crystalCell, self.atomList = H.readInfo(infoFile)
 
-            #Read data
-            self.spaceGroup, self.crystalCell, self.atomList = H.readInfo(infoFile)
-
-            # return wavelength, refList, sfs2, error, two-theta, and four-circle parameters
-            wavelength, refList, sfs2, error = S.readIntFile(observedFile, kind="int", cell=self.crystalCell)
-            self.wavelength = wavelength
-            self.refList = refList
-            self.sfs2 = sfs2
-            self.error = error
-            self.tt = [H.twoTheta(H.calcS(self.crystalCell, ref.hkl), wavelength) for ref in refList]
-            self.backg = None
-            self.exclusions = []
-
-        #TODO: else, for powder data
+         # return wavelength, refList, sfs2, error, two-theta, and four-circle parameters
+        wavelength, refList, sfs2, error = S.readIntFile(observedFile, kind="int", cell=self.crystalCell)
+        self.wavelength = wavelength
+        self.refList = refList
+        self.sfs2 = sfs2
+        self.error = error
+        self.tt = [H.twoTheta(H.calcS(self.crystalCell, ref.hkl), wavelength) for ref in refList]
+        self.backg = None
+        self.exclusions = []
 
         self.reset()
 
@@ -62,18 +58,7 @@ class PycrysfmlEnvironment(Environment):
         #Set a range on the x value of the first atom in the model
         self.model.atomListModel.atomModels[0].z.value = 0.3
         self.model.atomListModel.atomModels[0].z.range(0,0.5)
-#        self.model.atomListModel.atomModels[0].B.range(0, 5)
-#        self.model.atomListModel.atomModels[1].B.range(0,5)
-#        self.model.atomListModel.atomModels[2].B.range(0,5)
-#        self.model.atomListModel.atomModels[3].z.range(0,0.5)
-#        self.model.atomListModel.atomModels[3].B.range(0,5)
-#        self.model.atomListModel.atomModels[4].B.range(0,5)
-#        self.model.atomListModel.atomModels[5].x.range(0,0.5)
-#        self.model.atomListModel.atomModels[5].y.range(0,0.5)
-#        self.model.atomListModel.atomModels[5].z.range(0,0.5)
-#        self.model.atomListModel.atomModels[5].B.range(0,5)
 
-        #TODO: clean up excess vars
         self.visited = []
         self.observed = []
         self.remainingActions = []
@@ -94,26 +79,21 @@ class PycrysfmlEnvironment(Environment):
         #Create a problem from the model with bumps,
         #then fit and solve it
         problem = bumps.FitProblem(model)
-#        print("before: ", lsqerr.stderr(problem.cov()))
         fitted = fitter.LevenbergMarquardtFit(problem)
         x, dx = fitted.solve()
-#        print(problem.chisq())
-#        print("after", lsqerr.stderr(problem.cov()))
-        return x, dx, lsqerr.stderr(problem.cov())
+
+        return x, dx, problem.chisq()
 
     def execute(self, actions):
 
         self.step += 1
 
+        # negative reward for repeat actions
         if self.state[actions] == 1:
-
-#            print(self.refList[actions.item()].hkl, self.step)
             self.totReward -= 0.15
-            return self.state, (self.step > 300), -0.15  #stop only if step > 200
+            return self.state, (self.step > 300), -0.15  #stop only if step > 300
         else:
             self.state[actions] = 1
-
-
 
         #No repeats
         self.visited.append(self.refList[actions.item()])
@@ -136,32 +116,21 @@ class PycrysfmlEnvironment(Environment):
         if len(self.visited) > 11:
 
             x, dx, chisq = self.fit(self.model)
-#            print(x, chisq)
-
-#            problem = bumps.FitProblem(self.model)
-#            stderr = lsqerr.stderr(problem.cov())
 
             if (self.prevChisq != None and chisq < self.prevChisq):
                 reward = 0.1
 
             self.prevChisq = chisq
 
-#            print(x,"\n", stderr,"\n", reward, "\n")
-
         self.totReward += reward
 
-        if (self.prevChisq != None and self.step > 50 and chisq < 50):
+        #stop early if chisq drops under certain threshold
+        if (self.prevChisq != None and self.step > 50 and chisq < 1):
             return self.state, True, 0.5
         if (len(self.remainingActions) == 0 or self.step > 300):
-#            print(self.model.atomListModel.atomModels[0].z.value, self.prevChisq, self.totReward, self.step)
             terminal = True
         else:
             terminal = False
-
-#        self.stateList.append(self.state.copy())
-#        fig = mpl.pyplot.pcolor(self.stateList, cmap="RdBu" )
-#        mpl.pyplot.savefig("state_space.png")
-
 
         return self.state, terminal, reward
 
@@ -171,18 +140,8 @@ class PycrysfmlEnvironment(Environment):
 
     @property
     def actions(self):
-
-        #TODO limit to remaining options (no repeats)
-        #TODO set up to have the hkls, so it can be generalized
         return dict(num_actions=len(self.refList), type='int')
 
-#    @actions.setter
-#    def actions(self, value):
-#        self._actions = value
-
-
-    #_______________________________________________
-    #TODO: read, understand, improve for my purposes
     @staticmethod
     def from_spec(spec,     kwargs):
         """
